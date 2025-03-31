c8cf4f8b0c75ad41ee6f4575dffa272f23f05b26
daeunhan <daeunhan0723@gmail.com>
2025-03-31 23:17:32 +0900

    init: project setting

---
+++ src/main/java/com/example/aitalk/AitalkApplication.java
@@ -0,0 +1,13 @@
+package com.example.aitalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AitalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AitalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aitalk/AitalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aitalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AitalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
