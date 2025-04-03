2366a596eac240b3f0606ade073c6d6550325fab
daeunhan <daeunhan0723@gmail.com>
2025-04-04 01:11:43 +0900

    fix: @Builder error

--- src/main/java/com/example/aitalk/member/MemberJoinResponseDTO.java
+++ src/main/java/com/example/aitalk/member/MemberJoinResponseDTO.java
@@ -1,15 +1,13 @@
 package com.example.aitalk.member;
 
 
-import lombok.Builder;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @Builder
 @Data
+@AllArgsConstructor // 모든 필드를 포함하는 생성자가 자동 생성
 public class MemberJoinResponseDTO {
 
     int statusCode; // 응답 상태 코드
