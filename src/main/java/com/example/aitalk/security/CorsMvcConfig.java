e1f178d03574f03db41c67c642d9006c76d045de
daeunhan <daeunhan0723@gmail.com>
2025-04-28 23:08:58 +0900

    fix: cors-setting

--- src/main/java/com/example/aitalk/security/CorsMvcConfig.java
+++ src/main/java/com/example/aitalk/security/CorsMvcConfig.java
@@ -12,7 +12,7 @@ public class CorsMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry corsRegistry) {
 
         corsRegistry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // 추후 FE와 협업할것을 염두에 두고 3000 포트 허용
+                .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // PATCH(일부만 업데이트)를 제외한 모든 http 메서드를 허용
                 .allowCredentials(true); // 쿠키 허용 (토큰을 이용한 방식의 로그인일 경우, 쿠키 사용을 해야해서 허용 처리)
     }
