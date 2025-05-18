a4610f3af6738e701a4ff86d6f3286d25a9bf54e
daeunhan <daeunhan0723@gmail.com>
2025-05-18 23:48:59 +0900

    feat: add front deploy url

--- src/main/java/com/example/aitalk/config/SecurityConfig.java
+++ src/main/java/com/example/aitalk/config/SecurityConfig.java
@@ -39,6 +39,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("https://ai-talkk.netlify.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
--- src/main/java/com/example/aitalk/member/MemberController.java
+++ src/main/java/com/example/aitalk/member/MemberController.java
@@ -7,7 +7,6 @@ import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
